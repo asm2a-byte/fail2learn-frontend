62e8cd53b2ad55d891a2f7be9b269a753d81ac10
ASMAA EL FAHLI <asmaa.oculus@gmail.com>
2025-08-24 08:48:54 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
+import './globals.css'
+
 export const metadata = {
-  title: "Fail2Learn",
-  description: "Experiments platform"
-};
+  title: 'Fail2LearnLab',
+  description: 'Transform failed experiments into smarter strategies',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
