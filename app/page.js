60957aef81bdc392519c3ee6a4eddc4a435cc281
ASMAA EL FAHLI <asmaa.oculus@gmail.com>
2025-08-24 06:20:02 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,17 @@
 
+export default function Home() {
+  return (
+    <main>
+      <h1>Fail2LearnLab</h1>
+      <p>Select a thematic:</p>
+      <ul>
+        <li>Pharma</li>
+        <li>Batteries</li>
+        <li>Materials</li>
+        <li>Chemistry</li>
+        <li>Biotech</li>
+        <li>Energy</li>
+      </ul>
+    </main>
+  );
+}
